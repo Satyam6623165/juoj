f69234e3f64347e07bf3ced85678146565280233
Satyam6623165 <satyam6623165@gmail.com>
2020-11-11 14:41:19 +0530

    initial commit

---
+++ src/main/java/com/onlineJudge/juoj/JuojApplication.java
@@ -0,0 +1,17 @@
+package com.onlineJudge.juoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.onlineJudge.codeforces"})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class JuojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuojApplication.class, args);
+	}
+}
---
+++ src/test/java/com/onlineJudge/juoj/JuojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineJudge.juoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
